79cb9eaafbf22739ca6f46fecf3cfc49697a759d
kamrujama <88245491+kamrujama@users.noreply.github.com>
2022-07-23 01:23:25 +0530

    Commit after changes

--- src/App.js
+++ src/App.js
@@ -1,93 +1,100 @@
-import React, { useState } from 'react'
-import './App.css';
-import Alert from './components/Alert';
-import About from './components/About';
-import HomePage from './components/HomePage';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+import React, { useState } from "react";
+import "./App.css";
+import Alert from "./components/Alert";
+import About from "./components/About";
+import HomePage from "./components/HomePage";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 import { Routes, Route } from "react-router-dom";
 
-
-
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const [txtColor, setTxtColor] = useState(null);
 
-  const [mode, setMode] = useState('light')
-  const [alert, setAlert] = useState(null)
-  const [txtColor, setTxtColor] = useState(null)
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
 
-  const showAlert = (message, type)=> {
-      setAlert({
-        msg : message,
-        type : type
-      });
-
-      setTimeout(() => {
-        setAlert(null)
-      },2000);
-  }
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
 
   const setColors = () => {
-    if(txtColor === 'green'){
-      setTxtColor('green')
-       document.body.style.backgroundColor = 'green';
-       document.body.style.color = 'white'
+    if (txtColor === "green") {
+      setTxtColor("green");
+      document.body.style.backgroundColor = "green";
+      document.body.style.color = "white";
     }
-  }
+  };
 
-  const toggle = ()=> {
-
-    if(mode === 'green'){
-      setMode('green')
-      document.body.style.backgroundColor = 'green';
-      document.body.style.color = 'white'
-      showAlert("Green mode has been enabled", "success")
+  const toggle = () => {
+    if (mode === "green") {
+      setMode("green");
+      document.body.style.backgroundColor = "green";
+      document.body.style.color = "white";
+      showAlert("Green mode has been enabled", "success");
+    } else if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      document.body.style.color = "white";
+      document.title = "Utils - Dark Mode";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      document.title = "Utils - Light Mode";
+      showAlert("Light mode has been enabled", "success");
     }
-
-   else if(mode === 'light'){
-       setMode('dark')
-       document.body.style.backgroundColor = '#042743';
-       document.body.style.color = 'white'
-       document.title = 'Utils - Dark Mode'
-       showAlert("Dark mode has been enabled", "success")
-   }
-
-   else{
-       setMode('light')
-       document.body.style.backgroundColor = 'white'
-       document.body.style.color = 'black'
-       document.title = 'Utils - Light Mode'
-       showAlert("Light mode has been enabled", "success")
-   }
-  }
+  };
 
   return (
     <>
+      {/* Navbar componenet */}
+      <Navbar
+        title="Text Editor"
+        txtColor={txtColor}
+        setColors={setColors}
+        aboutText="About"
+        mode={mode}
+        setMode={toggle}
+      />
 
-        {/* Navbar componenet */}
-        <Navbar title="Text Editor" txtColor = {txtColor} setColors = {setColors}  aboutText="About" mode={mode} setMode={toggle}/>
+      {/* Alert component */}
+      <Alert alertMsg={alert} />
 
-        {/* Alert component */}
-        <Alert  alertMsg={alert} />
+      {/* Text form component */}
+      {/* <TextForm showAlert = {showAlert} heading = "Enter the text to analyze" mode={mode} /> */}
 
-      
+      {/* About Component */}
+      {/* <About/> */}
 
-        {/* Text form component */}
-          {/* <TextForm showAlert = {showAlert} heading = "Enter the text to analyze" mode={mode} /> */}
-        
-        {/* About Component */}
-          {/* <About/> */}
-        
-        {/* <HomePage /> */}
+      {/* <HomePage /> */}
 
-        <Routes>
-         
-          <Route path="/HomePage" element={<HomePage mode={mode} />} />
-          <Route path="/textForm" element={<TextForm showAlert = {showAlert} heading = "Enter the text to analyze" mode={mode} />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      
+      {/* <Router> */}
+      <Routes>
+        {/* <Route exact path='/'>
+            <HomePage/>
+          </Route> */}
 
-        
+        <Route exact path="/Text-Utils" element={<HomePage mode={mode} />} />
+        <Route
+          path="/textForm"
+          element={
+            <TextForm
+              showAlert={showAlert}
+              heading="Enter the text to analyze"
+              mode={mode}
+            />
+          }
+        />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      {/* </Router> */}
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,104 +1,155 @@
-
-
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const handleUpperCase = () => {
+    // console.log("You clicked this text"+text)
+    if (text === text.toUpperCase() && text !== text.toLowerCase())
+      props.showAlert("Texts are already converted to uppercase.", "success");
+    else if (text.length !== 0) {
+      let upperText = text.toUpperCase();
+      setText(upperText);
+      props.showAlert("Text Converted to UpperCase", "success");
+    } else
+      props.showAlert(
+        "Box is empty, Nothing to convert in UpperCase",
+        "success"
+      );
+  };
 
-    const handleUpperCase = () => {
-        // console.log("You clicked this text"+text)
-        if(text === text.toUpperCase() && text !== text.toLowerCase())
-            props.showAlert("Texts are already converted to uppercase.","success")
+  const handleLowerCase = () => {
+    if (text === text.toLowerCase() && text !== text.toUpperCase())
+      props.showAlert("Texts are already in lowercase.", "success");
+    else if (text.length !== 0) {
+      let lowerText = text.toLowerCase();
+      setText(lowerText);
+      props.showAlert("Text Converted to LowerCase", "success");
+    } else
+      props.showAlert(
+        "Box is empty. Nothing to convert in LowerCase",
+        "success"
+      );
+  };
 
-        else if(text.length !== 0){
-            let upperText = text.toUpperCase()
-            setText(upperText)
-            props.showAlert("Text Converted to UpperCase","success")
-        }
-        else
-            props.showAlert("Box is empty, Nothing to convert in UpperCase","success")
-    }
+  const handleClear = () => {
+    if (text.length !== 0) {
+      let newText = "";
+      setText(newText);
+      props.showAlert("All Text in the box is cleared", "success");
+    } else
+      props.showAlert("Box is already cleared. Nothing to clear.", "success");
+  };
 
-    const handleLowerCase = () => {
-        if(text === text.toLowerCase() && text !== text.toUpperCase())
-            props.showAlert("Texts are already in lowercase.","success")
+  const handleCopy = () => {
+    // console.log(text)
+    if (text.length !== 0) {
+      let newText = document.getElementById("textForm");
+      newText.select();
+      navigator.clipboard.writeText(newText.value);
+      props.showAlert("All text in the box is copied to clipboard", "success");
+    } else props.showAlert("Box is empty. Nothing to copy.", "success");
+  };
 
-        else if(text.length !== 0){
-            let lowerText = text.toLowerCase()
-            setText(lowerText)
-            props.showAlert("Text Converted to LowerCase","success")
-        }
-       
-        else
-            props.showAlert("Box is empty. Nothing to convert in LowerCase","success")
-    }
+  const handleSpace = () => {
+    if (text.split(" ").length > 1) {
+      let newText = text.split(/[ ]+/);
+      setText(newText.join(" "));
+      props.showAlert("All Spaces are removed", "success");
+    } else if (text.split(" ").length === 1)
+      props.showAlert(
+        "There is only one word in the box. No Space can be removed.",
+        "success"
+      );
+    else props.showAlert("Box is empty. No Spaces removed.", "success");
+  };
 
-    const handleClear = () => {
-        if(text.length !== 0){
-            let newText = ''
-            setText(newText)
-            props.showAlert("All Text in the box is cleared","success")
-        }
-        else
-        props.showAlert("Box is already cleared. Nothing to clear.","success")
-    }
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
 
-    const handleCopy = ()=> {
-        // console.log(text)
-        if(text.length !== 0){
-            let newText = document.getElementById('textForm')
-            newText.select()
-            navigator.clipboard.writeText(newText.value)
-            props.showAlert("All text in the box is copied to clipboard","success")
-        }
-        else
-            props.showAlert("Box is empty. Nothing to copy.","success")
-    }
-
-    const handleSpace = ()=> {
-        if(text.split(' ').length > 1){
-            let newText = text.split(/[ ]+/);
-            setText(newText.join(" "))
-            props.showAlert("All Spaces are removed","success")
-        }
-        else if(text.split(' ').length === 1)
-            props.showAlert("There is only one word in the box. No Space can be removed.","success")
-        else
-            props.showAlert("Box is empty. No Spaces removed.","success")
-    }
-
-    const handleOnChange = (event) => {
-        setText(event.target.value)
-    }
-
-    const [text, setText] = useState("")
+  const [text, setText] = useState("");
 
   return (
     <>
-    <div className="container">
+      <div className="container">
         <h2>{props.heading}</h2>
 
         <div className="mb-3">
-            <textarea className="form-control" value={text} id="textForm" onChange={handleOnChange} style={{backgroundColor : props.mode === 'dark' ? '#042743' : 'white', color :props.mode === 'light' ? 'black' : 'white' }} rows="8"></textarea>
+          <textarea
+            className="form-control"
+            value={text}
+            id="textForm"
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "#042743" : "white",
+              color: props.mode === "light" ? "black" : "white",
+            }}
+            rows="8"
+          ></textarea>
         </div>
 
-        <button className={`btn btn-${props.mode === 'light' ? 'primary' : 'dark'}`} onClick={handleUpperCase}>Convert to Uppercase</button>
-        <button className={`btn btn-${props.mode === 'light' ? 'primary' : 'dark'} m-2`} onClick={handleLowerCase}>Convert to Lowercase</button>
-        <button className={`btn btn-${props.mode === 'light' ? 'primary' : 'dark'} m-2`} onClick={handleClear}>Clear</button>
-        <button className={`btn btn-${props.mode === 'light' ? 'primary' : 'dark'} m-2`} onClick={handleCopy}>Click to Copy</button>
-        <button className={`btn btn-${props.mode === 'light' ? 'primary' : 'dark'} m-2`} onClick={handleSpace}>Remove Extra Space</button>
+        <button
+          className={`btn btn-${props.mode === "light" ? "primary" : "dark"}`}
+          onClick={handleUpperCase}
+        >
+          Convert to Uppercase
+        </button>
+        <button
+          className={`btn btn-${
+            props.mode === "light" ? "primary" : "dark"
+          } m-2`}
+          onClick={handleLowerCase}
+        >
+          Convert to Lowercase
+        </button>
+        <button
+          className={`btn btn-${
+            props.mode === "light" ? "primary" : "dark"
+          } m-2`}
+          onClick={handleClear}
+        >
+          Clear
+        </button>
+        <button
+          className={`btn btn-${
+            props.mode === "light" ? "primary" : "dark"
+          } m-2`}
+          onClick={handleCopy}
+        >
+          Click to Copy
+        </button>
+        <button
+          className={`btn btn-${
+            props.mode === "light" ? "primary" : "dark"
+          } m-2`}
+          onClick={handleSpace}
+        >
+          Remove Extra Space
+        </button>
+      </div>
 
-    </div>
-    
-    <div className="container my-2">
+      <div className="container my-2">
         <h3>Your Text Summary</h3>
-        <p className='lead'><b>{text.split(" ").length - 1}</b> word and <b>{text.length}</b> characters</p>
-        <p className='lead'> <b>{0.008 * text.split(" ").length}</b> minute it will take to read</p>
-        <p className='lead'><b>{text.split(". ").length - 1}</b> Sentences</p>
-        <p className='lead'><b>{text.split("\n").length - 1}</b> Paragraphs</p>
+        <p className="lead">
+          <b>{text.split(" ").length - 1}</b> word and <b>{text.length}</b>{" "}
+          characters
+        </p>
+        <p className="lead">
+          {" "}
+          <b>{0.008 * text.split(" ").length}</b> minute it will take to read
+        </p>
+        <p className="lead">
+          <b>{text.split(". ").length - 1}</b> Sentences
+        </p>
+        <p className="lead">
+          <b>{text.split("\n").length - 1}</b> Paragraphs
+        </p>
         <h3>Your Text Preview</h3>
-        <p className='lead'>{text.length > 0 ? text : 'Enter your text in the above box for preview'}</p>
-    </div>
-
+        <p className="lead">
+          {text.length > 0
+            ? text
+            : "Enter your text in the above box for preview"}
+        </p>
+      </div>
     </>
-  )
+  );
 }
